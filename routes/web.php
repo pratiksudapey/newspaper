0e21b12b8f3cd6e301f3e505fcc455d389fb8834
dawa tamang <Dipesh527>
2021-08-30 21:41:13 +0545

    about created

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('frontend.index');
 });
+
+
+Route::get('/about', function() {
+    return view('frontend.about');
+});
+
