ea90a3317f86cf8eb7b74cbaade8fafc775f0df5
Pratik Shrestha (Sudapey) <60513331+pratiksudapey@users.noreply.github.com>
2021-08-30 23:20:06 +0545

    header updated finally

--- resources/views/templates/master_parts/header.blade.php
+++ resources/views/templates/master_parts/header.blade.php
@@ -55,9 +55,6 @@
                                         <li><a href="about.html">Home</a></li>
                                         <li><a href="categori.html">Category</a></li>
                                         <li><a href="/about">About</a></li>
-                                        <li><a href="index.html">Home</a></li>
-                                        <li><a href="categori">Category</a></li>
-                                        <li><a href="about.html">About</a></li>
                                         <li><a href="latest_news.html">Latest News</a></li>
                                         <li><a href="contact.html">Contact</a></li>
                                         <li><a href="#">Pages</a>
