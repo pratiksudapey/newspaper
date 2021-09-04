22261a752f177c88314d961a50b6ab587dfabbfb
Pratik Shrestha (Sudapey) <60513331+pratiksudapey@users.noreply.github.com>
2021-09-04 19:10:03 +0545

    some changes (#24)

---
+++ database/migrations/2021_09_02_195058_create_admin_contacts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->string('address_detail');
+            $table->bigInteger('contact');
+            $table->string('contact_detail');
+            $table->string('email');
+            $table->string('email_detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_contacts');
+    }
+}
